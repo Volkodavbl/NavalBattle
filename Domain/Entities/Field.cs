9debdb94d4b94f8e9de889d11287954648a5093b
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-12-15 00:34:11 +0300

    Added events in field

--- Domain/Entities/Field.cs
+++ Domain/Entities/Field.cs
@@ -3,6 +3,7 @@
     public class Field(int size, int shipCount)
     {
         public int Size { get; init; } = size;
-        public List<Ship> Ships { get; init; } = new List<Ship>(shipCount);
+        public List<Ship> Ships { get; init; } = new(shipCount);
+        public List<Point> Events { get; init; } = new(0);
     }
 }
