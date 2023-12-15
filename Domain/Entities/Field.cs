dc1a34c99a690fb8d208f570894ae9bbdc76302f
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-12-16 01:10:29 +0300

    Domain project and base models (#1)
    * Domain project and base models
    
    * Added events in field

---
+++ Domain/Entities/Field.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public class Field(int size, int shipCount)
+    {
+        public int Size { get; init; } = size;
+        public List<Ship> Ships { get; init; } = new(shipCount);
+        public List<Point> Events { get; init; } = new(0);
+    }
+}
