91cb4bbb8bf0a6aadb0f763f84bd72dc8249a5bf
Александр Скворцов <alexskvr03@gmail.com>
2023-12-16 23:46:14 +0300

    created room list component

--- Client/Services/Client.cs
+++ Client/Services/Client.cs
@@ -1,9 +1,24 @@
 ﻿using Domain.Contracts;
+using Domain.Entities;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Runtime.InteropServices;
 
 namespace Client.Services
 {
     public class ClientService : IClientContract
     {
+
+        public void ShowRoom(Room room)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public void ShowRoomList(List<Room> rooms)
+        {
+            throw new NotImplementedException();
+        }
+
         public string TestClientMethod()
         {
             return "TestClient";
