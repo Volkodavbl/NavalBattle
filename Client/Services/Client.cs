4de635ceaaeabc522c4575b2a18664551263bd9c
Alexander <alexskvr03@gmail.com>
2023-12-19 00:14:26 +0300

    Merge conflicts

--- Client/Services/Client.cs
+++ Client/Services/Client.cs
@@ -1,12 +1,33 @@
 ﻿using Domain.Contracts;
+using Domain.Entities;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Runtime.InteropServices;
 
 namespace Client.Services
 {
     public class ClientService : IClientContract
     {
-        public string TestClientMethod()
+
+        public event Action<List<Room>> RoomListChanged;
+        public event Action<Room, User> RoomChanged;
+        public event Action<string> ErrorOcured;
+        public Task Error(string message)
         {
-            return "TestClient";
+            ErrorOcured.Invoke(message);
+            return Task.CompletedTask;
+        }
+
+        public Task ShowRoom(Room room, User user)
+        {
+            RoomChanged.Invoke(room, user);
+            return Task.CompletedTask;
+
+        }
+
+        public Task ShowRoomList(List<Room> rooms)
+        {
+            RoomListChanged.Invoke(rooms);
+            return Task.CompletedTask;
         }
     }
 }
